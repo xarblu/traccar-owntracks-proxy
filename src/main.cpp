bdc19db51e8715bd00e1b105ac87cd9fc4bebb28
Xarblu <xarblu@protonmail.com>
2026-05-20 20:32:48 +0200

    it mostly seems to work I think
    Signed-off-by: Xarblu <xarblu@protonmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,20 @@
 #include <nlohmann/json_fwd.hpp>
 #include <iostream>
 #include <cstdint>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <chrono>
+#include <optional>
+#include <regex>
 
 /**
  * Very basic ISO 8601 UTC time to UNIX epoch parser
+ * with second accuracy
  */
-static inline std::int64_t parseISO8601UTC(std::string timeString) {
+static inline std::optional<std::int64_t> parseISO8601UTC(std::string timeString) {
     using std::operator""sv;
-    constexpr size_t fullLen{("YYYY-MM-DDTHH:MM:SSZ"sv).size()};
+    constexpr size_t minLen{("YYYY-MM-DDTHH:MM:SSZ"sv).size()};
     constexpr size_t startYear{(""sv).size()};
     constexpr size_t startMonth{("YYYY-"sv).size()};
     constexpr size_t startDay{("YYYY-MM-"sv).size()};
@@ -21,62 +25,133 @@ static inline std::int64_t parseISO8601UTC(std::string timeString) {
     constexpr size_t startMinute{("YYYY-MM-DDTHH:"sv).size()};
     constexpr size_t startSecond{("YYYY-MM-DDTHH:MM:"sv).size()};
 
-    if (timeString.size() != fullLen) {
-        return 0;
+    if (timeString.size() < minLen) {
+        return std::nullopt;
     }
 
-    std::chrono::year year{std::stoi(timeString.substr(startYear, 4))};
-    std::chrono::month month{static_cast<unsigned int>(std::stoi(timeString.substr(startMonth, 2)))};
-    std::chrono::day day{static_cast<unsigned int>(std::stoi(timeString.substr(startDay, 2)))};
+    try {
+        std::chrono::year year{std::stoi(timeString.substr(startYear, 4))};
+        std::chrono::month month{static_cast<unsigned int>(std::stoi(timeString.substr(startMonth, 2)))};
+        std::chrono::day day{static_cast<unsigned int>(std::stoi(timeString.substr(startDay, 2)))};
 
-    std::chrono::year_month_day date{year, month, day};
-    std::chrono::sys_days days{date};
+        std::chrono::year_month_day date{year, month, day};
+        std::chrono::sys_days days{date};
 
-    std::chrono::seconds seconds = days.time_since_epoch();
-    seconds += std::chrono::hours{std::stoi(timeString.substr(startHour, 2))};
-    seconds += std::chrono::minutes{std::stoi(timeString.substr(startMinute, 2))};
-    seconds += std::chrono::seconds{std::stoi(timeString.substr(startSecond, 2))};
+        std::chrono::seconds seconds = days.time_since_epoch();
+        seconds += std::chrono::hours{std::stoi(timeString.substr(startHour, 2))};
+        seconds += std::chrono::minutes{std::stoi(timeString.substr(startMinute, 2))};
+        seconds += std::chrono::seconds{std::stoi(timeString.substr(startSecond, 2))};
 
-    return seconds.count();
+        return seconds.count();
+    } catch (std::invalid_argument e) {
+        return std::nullopt;
+    }
 }
 
 int main(int argc, char *argv[]) {
     std::string server_addr{"0.0.0.0"};
     uint server_port{8080};
-    std::string owntracks_url{};
+    std::optional<std::string> owntracks_url{};
 
-    for (int i = 0; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         std::string_view arg{argv[i]};
 
         if (arg == "--server-addr") {
-            if (i + 1 >= argc) return 1;
-            server_addr = argv[i+1];
+            if (i + 1 >= argc) {
+                std::cerr << "Argument " << arg << " requires a value\n";
+                return 1;
+            }
+            server_addr = argv[++i];
         }
 
-        if (arg == "--server-port") {
-            if (i + 1 >= argc) return 1;
-            server_port = std::stoi(argv[i+1]);
+        else if (arg == "--server-port") {
+            if (i + 1 >= argc) {
+                std::cerr << "Argument " << arg << " requires a value\n";
+                return 1;
+            }
+            try {
+                server_port = std::stoi(argv[++i]);
+            } catch (std::invalid_argument e) {
+                std::cerr << "Invalid integer for argument: " << arg << "\n";
+                return 1;
+            }
         }
 
-        if (arg == "--owntracks-url") {
-            if (i + 1 >= argc) return 1;
-            owntracks_url = argv[i+1];
+        else if (arg == "--owntracks-url") {
+            if (i + 1 >= argc) {
+                std::cerr << "Argument " << arg << " requires a value\n";
+                return 1;
+            }
+            owntracks_url = argv[++i];
+        }
+
+        else {
+            std::cerr << "Unknown argument: " << arg << "\n";
+            return 1;
         }
     }
 
-    if (owntracks_url.empty()) {
+    if (!owntracks_url.has_value()) {
         std::cerr << "--owntracks-url is required\n";
+        return 1;
     }
+
+    std::optional<std::string> owntracks_host{};
+    std::optional<std::string> owntracks_netloc{};
+
+    std::regex url_regex{"^(https?://[^/]+)(/.+)?$"};
+    std::smatch matches{};
+    std::regex_match(owntracks_url.value(), matches, url_regex);
+
+    bool skipFirst{true};
+    for (auto &match : matches) {
+        if (skipFirst) {
+            skipFirst = false;
+            continue;
+        }
+
+        if (!owntracks_host.has_value()) {
+            owntracks_host = match;
+            continue;
+        }
+        if (!owntracks_netloc.has_value()) {
+            owntracks_netloc = match;
+            continue;
+        }
+    }
+
+    if (!owntracks_host.has_value()) {
+        std::cerr << "Malformed URL: " << owntracks_url.value() << "\n";
+        return 1;
+    }
+    if (!owntracks_netloc.has_value()) {
+        std::cerr << "Using default location /\n";
+        owntracks_netloc = "/";
+    }
+
+    std::cout << "Forwarding requests to: " << owntracks_host.value() << owntracks_netloc.value() << "\n";
 
     httplib::Server server{};
-    httplib::Client client{"https://dawarich.gaydragon.zone/api/v1/owntracks/points"};
+    httplib::Client client{owntracks_host.value()};
     std::mutex client_mutex{};
 
-    server.Post("/api/v1/traccar/points", [&client, &client_mutex](const httplib::Request &req, httplib::Response &res) {
+    server.Post("/api/v1/traccar/points", [&](const httplib::Request &req, httplib::Response &res) {
+        if (!nlohmann::json::accept(req.body)) {
+            res.status = httplib::StatusCode::BadRequest_400;
+            res.set_content("Invalid JSON\n", "text/plain");
+            return;
+        }
+
+        std::optional<std::string> api_key{};
+        if (req.has_param("api_key")) {
+            api_key = req.get_param_value("api_key");
+        }
+
         nlohmann::json traccar_json = nlohmann::json::parse(req.body);
         nlohmann::json owntracks_json{};
 
-        std::cout << traccar_json << "\n";
+        std::cout << "New request:\n";
+        std::cout << "Traccar:\n" << traccar_json.dump(2) << "\n";
 
         // validate rough structure
         if (!traccar_json["location"].is_object()) {
@@ -105,34 +180,91 @@ int main(int argc, char *argv[]) {
             return;
         }
 
+        // remap based on
+        // https://owntracks.org/booklet/tech/json/#_typelocation
+
+        // required type
+        owntracks_json["_type"] = "location";
+
         // ID of the device
         owntracks_json["tid"] = traccar_json["device_id"];
 
-        // Timestamp in Unix epoch time
-        owntracks_json["location"]["tst"] = parseISO8601UTC(traccar_json["location"]["timestamp"]);
-        // Human-readable timestamp of the location fix
-        owntracks_json["location"]["isotst"] = traccar_json["location"]["timestamp"];
+        // TODO: remap location.activity
 
-        // Longitude coordinate
-        owntracks_json["location"]["lon"] = traccar_json["location"]["coords"]["longitude"];
-        // Latitude coordinate
-        owntracks_json["location"]["lat"] = traccar_json["location"]["coords"]["latitude"];
-        // Accuracy of position in meters
-        owntracks_json["location"]["acc"] = traccar_json["location"]["coords"]["accuracy"];
-        // Altitude in meters
-        owntracks_json["location"]["alt"] = traccar_json["location"]["coords"]["altitude"];
+        // location.battery
+        // Battery status | Traccar: bool | Owntracks: int, 0=unknown, 1=unplugged, 2=charging, 3=full
+        owntracks_json["bs"] = traccar_json["location"]["battery"]["is_charging"] ? 2 : 1;
+        // Battery level | Traccar: float [0, 1] | Owntracks: int [0, 100]
+        if (auto &val = traccar_json["location"]["battery"]["level"]; val.is_number()) {
+            owntracks_json["batt"] = val.get<float>() * 100.0;
+        }
 
-        // Device battery level (percentage)
-        owntracks_json["location"]["batt"] = traccar_json["location"]["battery"]["level"];
-        // Battery status (0=unknown, 1=unplugged, 2=charging, 3=full)
-        owntracks_json["location"]["bs"] = traccar_json["location"]["battery"]["is_charging"] ? 2 : 1;
+        // location.coords
+        // Accuracy | Traccar: float, meters | Owntracks: int, meters
+        if (auto &val = traccar_json["location"]["coords"]["accuracy"]; val.is_number()) {
+            owntracks_json["acc"] = val;
+        }
+        // Altitude | Traccar: float, meters | Owntracks: int, meters
+        if (auto &val = traccar_json["location"]["coords"]["altitude"]; val.is_number()) {
+            owntracks_json["alt"] = val;
+        }
+        // Heading | Traccar: float, degrees | Owntracks: int, degrees
+        if (auto &val = traccar_json["location"]["coords"]["heading"]; val.is_number()) {
+            owntracks_json["cog"] = val;
+        }
+        // Latitude coordinate | Traccar: float | Owntracks: float
+        if (auto &val = traccar_json["location"]["coords"]["latitude"]; val.is_number()) {
+            owntracks_json["lat"] = val;
+        }
+        // Longitude coordinate | Traccar: float | Owntracks: float
+        if (auto &val = traccar_json["location"]["coords"]["longitude"]; val.is_number()) {
+            owntracks_json["lon"] = val;
+        }
+        // Velocity | Traccar: float, km/h (?) | Owntracks: int, km/h
+        if (auto &val = traccar_json["location"]["coords"]["speed"]; val.is_number()) {
+            owntracks_json["vel"] = val;
+        }
 
-        // Motion state (0=stopped, 1=moving)
-        owntracks_json["location"]["m"] = traccar_json["location"]["is_moving"];
-        // Internal message type (usually "location")
-        owntracks_json["location"]["_type"] = "location";
+        // location.*
+        // Timestamp of the location fix | Traccar: string, ISO8601UTC | Owntracks: int, Unix Epoch
+        if (auto &val = traccar_json["location"]["timestamp"]; val.is_string()) {
+            owntracks_json["isotst"] = val;
+            if (auto tst = parseISO8601UTC(val); tst.has_value()) {
+                owntracks_json["tst"] = tst;
+            }
+        }
 
-        res.set_content(owntracks_json.dump(), "application/json");
+        std::cout << "Owntracks:\n" << owntracks_json.dump(2) << "\n";
+
+        std::string params{};
+        if (api_key.has_value()) {
+            params += "?api_key=" + api_key.value();
+        }
+
+        {
+            std::lock_guard lock{client_mutex};
+            auto client_res = client.Post(owntracks_netloc.value() + params,
+                                          owntracks_json.dump(),
+                                          "application/json");
+
+            if (client_res) {
+                if (client_res->status == httplib::StatusCode::OK_200) {
+                    std::cerr << "Successfully forwarded to Owntracks endpoint\n";
+                } else {
+                    std::cerr << "Owntracks endpoint returned " << client_res->status << "\n";
+                }
+
+                // forward result back
+                res.status = client_res->status;
+                res.set_content(client_res->body, client_res->get_header_value("Content-Type", "text/plain"));
+                return;
+            } else {
+                std::cerr << "Connecting to Owntracks endpoint failed: " << client_res.error() << "\n";
+                res.status = httplib::StatusCode::BadGateway_502;
+                res.set_content("Connecting to Owntracks endpoint failed\n", "text/plain");
+                return;
+            }
+        }
     });
 
     std::cout << "Starting server\n";
